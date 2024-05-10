d35445d49fdbfb806f04283139accf97bb668441
Kaki Kagatan <kakikagatan@outlook.com>
2024-05-10 02:37:49 -0700

    added basic sass files and added basic styles to nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import './App.css'
 import AppRouter from './routers/AppRouter'
 
 function App() {
-
 
   return (
     <>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom"
 const Nav = () => {
   return (
     <nav>
-        <ul>
+        <ul className="nav-links">
             <li><NavLink to="/">Home</NavLink></li>
             <li><NavLink to="/favourites">Favourites</NavLink></li>
             <li><NavLink to="/about">About</NavLink></li>
