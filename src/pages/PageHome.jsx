958a1018d10f05e7e219051816c187bcc0bef5cb
Kaki Kagatan <kakikagatan@outlook.com>
2024-05-14 08:52:22 -0700

    improved load more function

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -9,13 +9,13 @@ import { appTitle, categories } from "../global/global";
 const PageHome = () => {
 	const [movieList, setMovieList] 		    = useState([]);
 	const [currentCategory, setCurrentCategory] = useState("now_playing");
-	const [loadMore, setLoadMore] 			    = useState(false);
 	const [currentPage, setCurrentPage] 		= useState(1);
 
 	const favs = useSelector((state) => state.favs.items);
 
 	useEffect(() => {
 		document.title = "Home | " + appTitle;
+		setCurrentPage(1);
 		const fetchMovies = async (category) => {
 
 			const response = await fetch(`https://api.themoviedb.org/3/movie/${category}?language=en-US&page=${currentPage}`, {
@@ -27,27 +27,15 @@ const PageHome = () => {
 	
 			let data = await response.json();
 	
-			console.log(data);
-
-			if(loadMore) {
-				// data.results.map((movie) => {
-				// 	setMovieList(...movieList, movie);
-				// })
-
-				setMovieList(movieList => ([...movieList, ...data.results]));
-				console.log(movieList);
-				setLoadMore(false);
-			} else {
-				setMovieList(data.results.slice(0, 18));
-			}		
+			setMovieList(data.results.slice(0, 18));
 		}
 
 		fetchMovies(currentCategory);
-	}, [currentCategory, loadMore]);
+	}, [currentCategory]);
 
 	// Chaning tabs 
 	const changeCategories = (category) => {
-		setCurrentPage(1);
+		
 		if (category.category == 'Now Playing') {
 			setCurrentCategory('now_playing');
 		} else if (category.category == 'Upcoming') {
@@ -59,9 +47,19 @@ const PageHome = () => {
 		}
 	};
 
-	const load = () => {
-		setLoadMore(true);
-		setCurrentPage(currentPage + 1);
+	const loadMore = async () => {
+		const newPage = currentPage + 1;
+		setCurrentPage(newPage);
+		const response = await fetch(`https://api.themoviedb.org/3/movie/${currentCategory}?language=en-US&page=${newPage}`, {
+			headers: {
+				accept: 'application/json',
+				Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMjkzZTM0ZGNiZjg1NGEyZGMxYzE1ZDlkNDk2ODA2MSIsInN1YiI6IjY2MzUyN2QzMzU4ZGE3MDEyYTU1NjMzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WAbBbeAHgieYj7ZUUoUsFfA5cUTRs3ayB3NKYkhQxFM'
+			}
+		});
+
+		let data = await response.json();
+
+		setMovieList(movieList => ([...movieList, ...data.results.slice(0, 18)]));
 	}
 
 	return (
@@ -70,7 +68,6 @@ const PageHome = () => {
 				<ul>
 					{Object.keys(categories).map((category, i) => (
 						<li key={i} 
-							style={i == currentCategory ? {color: 'red'} : {color: 'blue'}}
 							className="tab" 
 							onClick={() => {changeCategories({ category })}}>
 							{category}
@@ -88,7 +85,7 @@ const PageHome = () => {
 						/>
 					})}
 				</div>
-				<button className='load-more-btn' onClick={() => load()}>Load More</button>
+				<button className='load-more-btn' onClick={() => {loadMore();}}>Load More</button>
 			</section>
 		</>
 	);
