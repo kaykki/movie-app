c0f7ae2775056b1ad6217cb77471f1330c5e8e7c
KyleThomsonn <156140131+KyleThomsonn@users.noreply.github.com>
2024-05-13 21:36:05 -0700

    created single movie info card and started styling

--- src/pages/PageMovieInfo.jsx
+++ src/pages/PageMovieInfo.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
 import MovieCard from '../components/MovieCard';
 import isFav from '../utilities/isFav';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { appTitle } from '../global/global';
+import FavButton from '../components/FavButton';
+import InfoCard from '../components/InfoCard';
 
 function MovieInfo() {
 
   const { id } = useParams(); 
   const [movie, setMovie] = useState(null);
+  const [movieCast, setMovieCast] = useState(null);
   const favs = useSelector((state) => state.favs.items);
+
+
+  useEffect(() => {
+		document.title = "Movie Information | " + appTitle;
+	}, []);
  
   useEffect(() => {
 
@@ -27,20 +35,50 @@ function MovieInfo() {
     }
       };
 
+      
+      const fetchCast = async () => {
+        const apiKey = '2e0de9d682ff6404a82153a83be192cf';
+        const url = `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apiKey}&language=en-US&page=1`;
+        
+        try {
+          const response = await fetch(url);
+          const data = await response.json();
+          console.log(data)
+          setMovieCast(data);
+        } catch (error) {
+          console.error('Error fetching data: ', error);
+        }
+      };
+      
       fetchMovie();
+      fetchCast();
+
+
   }, []);
 
   return (
     <div>
-      {movie ? (
-         <MovieCard
-              key={movie.id}
-              movie={movie}
-              isFav={isFav(favs, null, movie.id)}
-              />
+      {movie && movieCast ? (
+        <div>
+        <InfoCard 
+        key={movie.id}
+        movie={movie}
+        isFav={isFav(favs, null, movie.id)}
+        />
+        <section>
+          <h2>Cast</h2>
+          {movieCast.cast.slice(0, 8).map(member => (
+            <div key={member.id}>
+            <img src={`https://image.tmdb.org/t/p/w500${member.profile_path}`} alt={member.name} />
+            <h3>{member.name}</h3>
+            <p>{member.character}</p>
+            </div>
+          ))}
+        </section>
+        </div>
       ) : 
       <div>
-        <p>sadddd no movie</p>
+        <p>No movie to display.</p>
       </div>
       }
     </div>
