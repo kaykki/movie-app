92517ba1acac623e926c95df1008f04d8bf78eba
Kaki Kagatan <kakikagatan@outlook.com>
2024-05-27 02:02:53 -0700

    missing-poster.png: added a image that replaces missing poster_paths for movies

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,6 +4,7 @@ import { addFav, deleteFav } from '../features/favs/favsSlice';
 import React from 'react'
 import FavButton from './FavButton';
 import { Link } from 'react-router-dom';
+import missingPoster from '../images/missing-poster.png';
 
 function MovieCard({ movie, isFav }) {
 
@@ -24,7 +25,9 @@ function MovieCard({ movie, isFav }) {
             <div className='movie-poster' 
                  onMouseEnter={() => setIsHovered(true)}
                  onMouseLeave={() => setIsHovered(false)}>
-                <img src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`} alt={movie.title} className="movie-image" />
+                <img src={movie.poster_path ? `https://image.tmdb.org/t/p/w400${movie.poster_path}`: missingPoster} 
+                     alt={movie.title} 
+                     className="movie-image" />
                 <div className='hover-movie' style={ isHovered ? {opacity: 1} : {opacity: 0}}>
                     <p>{movie.overview.slice(0, 175)}...</p>
                     <Link className='more-info-btn' to={`/movieinfo/${movie.id}`}>More Info</Link>
