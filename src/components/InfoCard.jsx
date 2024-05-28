0166e6ecc2f0bc425cd61f143044df9374a9765c
KyleThomsonn <156140131+KyleThomsonn@users.noreply.github.com>
2024-05-27 19:32:49 -0700

    added "no poster" img for movies with no poster

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { addFav, deleteFav } from '../features/favs/favsSlice';
 import React from 'react'
 import FavButton from './FavButton';
+import missingPoster from  '../images/missing-poster.png'
 
 function InfoCard({ movie, isFav }) {
     const dispatch = useDispatch();
@@ -19,7 +20,7 @@ function InfoCard({ movie, isFav }) {
             <img className='info-backdrop' src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`} alt={movie.title} />
 
             <section className='movie-info-text'>
-                <img className='info-poster' src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                <img className='info-poster' src={movie.poster_path ? `https://image.tmdb.org/t/p/w400${movie.poster_path}`: missingPoster} alt={movie.title} />
 
                 <div>
                     <h2 className='movie-info-title'>{movie.title}</h2>
