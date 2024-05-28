0db352b2059bf57ea2938ae0ebf44d4d6a958b68
dannykim <156135266+dannykim113@users.noreply.github.com>
2024-05-27 22:50:28 -0700

    added HeroSlide.jsx component and responsive design

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import MovieCard from '../components/MovieCard';
+import HeroSlide from '../components/HeroSlide';
 import isFav from '../utilities/isFav';
 import { useSelector } from 'react-redux';
 import { appTitle, categories } from "../global/global";
-import { Link } from 'react-router-dom';
 
 const PageHome = () => {
 	const [movieList, setMovieList] = useState([]);
@@ -52,110 +52,14 @@ const PageHome = () => {
 		return newPage;
 	}
 
-	// hero-slide
-	const [movies, setMovies] = useState([]);
-	const [currentIndex, setCurrentIndex] = useState(0);
-	const [translateValue, setTranslateValue] = useState(0);
 
-	useEffect(() => {
-		const fetchNowPlaying = async () => {
-			const apiKey = '2e0de9d682ff6404a82153a83be192cf';
-			const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
-			const options = {
-				method: 'GET',
-				headers: {
-					'Content-Type': 'application/json',
-					'accept': 'application/json'
-				}
-			};
-
-			try {
-				const response = await fetch(url, options);
-				const { results } = await response.json();
-				setMovies(results.slice(0, 10));
-			} catch (error) {
-				console.error('Error fetching data: ', error);
-			}
-		};
-
-		fetchNowPlaying();
-	}, []);
-
-	useEffect(() => {
-		const interval = setInterval(() => {
-			slideRight();
-		}, 6000);
-		return () => clearInterval(interval);
-	}, [currentIndex, movies.length]);
-
-	const slideWidth = () => {
-		return document.querySelector('.movie-card').clientWidth;
-	};
-
-	const slideRight = () => {
-		if (currentIndex === movies.length - 1) {
-			setCurrentIndex(0);
-			setTranslateValue(0);
-		} else {
-			setCurrentIndex(currentIndex + 1);
-			setTranslateValue(translateValue - slideWidth());
-		}
-	};
-
-	const nextSlide = () => {
-		slideRight();
-	};
-
-	const prevSlide = () => {
-		if (currentIndex === 0) {
-			setCurrentIndex(movies.length - 1);
-			setTranslateValue(-(movies.length - 1) * slideWidth());
-		} else {
-			setCurrentIndex(currentIndex - 1);
-			setTranslateValue(translateValue + slideWidth());
-		}
-	}
-
-	const myTransValue = translateValue * 3;
 
 
 	return (
 		<main>
-			<section className="hero-slide">
-				<div className="slideshow-container">
-					<div
-						className="movie-slider"
-						style={{
-							transform: `translateX(${myTransValue}px)`,
-							transition: 'transform ease-out 0.5s'
-						}}
-					>
-						{movies.map((movie) => (
-							<div key={movie.id} className="movie-cardy">
-								<div className="movie-poster">
-									<img
-										src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-										alt={movie.title}
-										className="movie-imagey"
-									/>
-								</div>
-								<div className="movie-infor">
-									<h2 className="movie-title">{movie.title}</h2>
-									<p className="movie-overview">{movie.overview}</p>
-									<Link className='more-info-btn' to={`/movieinfo/${movie.id}`}>MORE INFO</Link>
-								</div>
-							</div>
-						))}
-					</div>
-					<button onClick={prevSlide} className="prev">
-						&#10094;
-					</button>
-					<button onClick={nextSlide} className="next">
-						&#10095;
-					</button>
-				</div>
-			</section>
-
+			<div>
+				<HeroSlide />
+			</div>
 			<nav className="tab-nav">
 				<ul>
 					{categories.map((category) => (
