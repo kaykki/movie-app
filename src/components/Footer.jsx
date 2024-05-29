b6db3568bf8639193023d70fbaeefa782f24711c
dannykim <156135266+dannykim113@users.noreply.github.com>
2024-05-28 22:32:49 -0700

    removed social media icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,6 @@ import { getYear } from '../utilities/utilities';
 import { authors } from '../global/global';
 import React from 'react';
 import seenemaLogo from '../assets/seenema.svg';
-import emailIcon from '../assets/email.svg';
-import facebookIcon from '../assets/facebook.svg';
-import instagramIcon from '../assets/instagram.svg';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
@@ -25,26 +22,7 @@ const Footer = () => {
         <img src={seenemaLogo} alt="Seenema Logo" className="footer-logo" />
       </Link>
       <p>&copy; {getYear() + " by " + authors} </p>
-      <div className="icon-wrapper">
-        <img
-          src={emailIcon}
-          alt="Email Icon"
-          className="email-icon"
-          onClick={emailClick}
-        />
-        <img
-          src={facebookIcon}
-          alt="Facebook Icon"
-          className="facebook-icon"
-          onClick={facebookClick}
-        />
-        <img
-          src={instagramIcon}
-          alt="Instagram Icon"
-          className="instagram-icon"
-          onClick={instagramClick}
-        />
-      </div>
+
     </footer>
   )
 }
