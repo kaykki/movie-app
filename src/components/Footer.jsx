2e5cfdaf776593f57411ff7eb5dec76227065240
Kaki Kagatan <kakikagatan@outlook.com>
2024-06-01 20:46:49 -0700

    Footer: reformatted

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,27 +6,16 @@ import { Link } from 'react-router-dom';
 import { appTitle } from '../global/global';
 
 const Footer = () => {
+    return (
+        <footer>
+            <Link className='logo' to="/">
+                <img src={logo} alt="Seenema Logo" />
+                <h1>{appTitle}</h1>
+            </Link>
+            <p>&copy; {getYear() + " by " + authors} </p>
 
-  const emailClick = () => {
-    window.location.href = 'https://mail.google.com';
-  }
-  const facebookClick = () => {
-    window.location.href = 'https://facebook.com';
-  }
-  const instagramClick = () => {
-    window.location.href = 'https://instagram.com';
-  }
-
-  return (
-    <footer>
-      <Link className='logo' to="/">
-        <img src={logo} alt="Seenema Logo" />
-        <h1>{appTitle}</h1>
-      </Link>
-      <p>&copy; {getYear() + " by " + authors} </p>
-
-    </footer>
-  )
+        </footer>
+    )
 }
 
 export default Footer
