7c7527cd2ba4a6c153b5cd594aab7d8d7a99736f
Yasin Colak <yasincolak35@gmail.com>
2024-06-02 13:22:32 -0700

    page-about completed

--- src/pages/PageAbout.jsx
+++ src/pages/PageAbout.jsx
@@ -19,38 +19,38 @@ const PageAbout = () => {
                     <h2>About</h2>
                     <p className="about-text">"Seenema" is an application that allows users to easily search, favorite, and find other valuable information about movies around the world.</p>
                     <p className="about-text">This product uses the TMDb API but is not endorsed or certified by TMDb. This application uses the TMDb API.
-                        This application was created for educational purposes only by;
-                        <div className="developers">
-                            <article>
-                                <p>Kaki Kagatan</p>
-                                <p>
-                                    <SocialIcon url="https://www.linkedin.com/in/kaki-kagatan-a2ab5a18a/" />
-                                    <SocialIcon url="https://github.com/kaykki" bgColor="#C996CC" />
-                                </p>
-                            </article>
-                            <article>
-                                <p>Danny Kim</p>
-                                <p><SocialIcon url="https://www.linkedin.com/in/danny-kim-8150262a9/" />
-                                    <SocialIcon url="https://github.com/dannykim113" bgColor="#C996CC" />
-                                </p>
-                            </article>
-                            <article>
-                                <p>Yasin Colak</p>
-                                <p>
-                                    <SocialIcon url="https://www.linkedin.com/in/yasin-colak-a95980133/" />
-                                    <SocialIcon url="https://github.com/yasincoolak" bgColor="#C996CC" />
-                                </p>
-                            </article>
-                            <article>
-                                <p>Kyle Thomson</p>
-                                <p>
-                                    <SocialIcon url="https://www.linkedin.com/in/kyle-thomson-a067002a3/" />
-                                    <SocialIcon url="https://github.com/KyleThomsonn" bgColor="#C996CC" />
-                                </p>
-                            </article>
-
-                        </div>
-                    </p>
+                        This application was created for educational purposes only by;</p>
+                    <div className="developers">
+                        <article>
+                            <p>Kaki Kagatan</p>
+                            <section className="social-icons">
+                                <SocialIcon url="https://www.linkedin.com/in/kaki-kagatan-a2ab5a18a/" />
+                                <SocialIcon url="https://github.com/kaykki" bgColor="#C996CC" />
+                            </section>
+                        </article>
+                        <article>
+                            <p>Danny Kim</p>
+                            <section className="social-icons">
+                                <SocialIcon url="https://www.linkedin.com/in/danny-kim-8150262a9/" />
+                                <SocialIcon url="https://github.com/dannykim113" bgColor="#C996CC" />
+                            </section>
+                        </article>
+                        <article>
+                            <p>Yasin Colak</p>
+                            <section className="social-icons">
+                                <SocialIcon url="https://www.linkedin.com/in/yasin-colak-a95980133/" />
+                                <SocialIcon url="https://github.com/yasincoolak" bgColor="#C996CC" />
+                            </section>
+                        </article>
+                        <article>
+                            <p>Kyle Thomson</p>
+                            <section className="social-icons">
+                                <SocialIcon url="https://www.linkedin.com/in/kyle-thomson-a067002a3/" />
+                                <SocialIcon url="https://github.com/KyleThomsonn" bgColor="#C996CC" />
+                            </section>
+                        </article>
+                    </div>
+                    
                     <img src={TmdbIcon} alt="Tmdb logo" width="100" />
                 </article>
             </div>
